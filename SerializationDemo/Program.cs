cd136758ef7cbc29bd5aef7b755a2672253dc632
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-19 08:42:53 +0530

    done

--- SerializationDemo/Program.cs
+++ SerializationDemo/Program.cs
@@ -11,31 +11,16 @@ namespace SerializationDemo
     {
         static void Main(string[] args)
         {
-            //  Serialization formatters should implement IFormatter interface
-            //  Type fidelity is preserved in binary serialization, but not in Xml serialization
-            //  Custom types such as classes, generic classes, enums, structs can be serialized
-            //  This should be done by applying [Serializable] attribute, failing to which
-            //  produces SerializationException.
+            //  Xml serialization is done by DataContractSerializer
+            //  DataContractSerializer was released with WCF and more attuned with messaging architecture
+            //  However, it also works with Serializable attributes. Surprisingly, it works for public properties
+            //  that have no attributes. Another supported interface is the IXmlSerializable
 
-            //  All primitive types, non-generic and generic collection types, delegates, and enums
-            //  are serializable as they already have the [Serializable] attribute already applied.
+            //  In contrast with binary serialization, DataContracts are opt-in. This means, the required fields
+            //  should be explicitly marked for serialization. The reason for this is to allow the
+            //  flexible serialization under abstraction needs.
 
-            //  All fields irrespective of their access-level are serialized, Properties are serialized
-            //  by actually serializing their backing fields. However, the problem may surface for auto properties
-            //  The reason is the backing field is generated at the runtime by the compiler and this may be different
-            //  for the deserialization, particularly if deserialization happens outside the assembly.
-            //  At any rate, this automatic property serialization cannot be relied upon.
-            //  In such cases, ISerializable is necessary.
 
-            //  Child classes do not inherit the Serializable of the parent. Apply explicitly.
-
-            //  If the base-class is not marked as Serializable, then the child class cannot apply
-            //  Serializable either! It could be due to a design decision or the base class implementer
-            //  forgot. In such cases the implementation of ISerializable interface becomes a necessity.
-
-            //  Please note that object is marked as Serializable!
-
-            //  Also note that deserialization bypasses the constructor and initializer mechanisms!
         }
     }
 }
