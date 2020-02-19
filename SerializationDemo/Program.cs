0338d11c37067e55e8bb7111b74de1f8c797f936
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-19 05:51:04 +0530

    done

--- SerializationDemo/Program.cs
+++ SerializationDemo/Program.cs
@@ -10,6 +10,11 @@ namespace SerializationDemo
     {
         static void Main(string[] args)
         {
+            //  Serialization formatters should implement IFormatter interface
+            //  Type fidelity is preserved in binary serialization, but not in Xml serialization
+            //  Custom types such as classes, generic classes, enums, structs can be serialized
+            //  This should be done by applying [Serialization] attribute, failing to which
+            //  produces SerializationException.
         }
     }
 }
